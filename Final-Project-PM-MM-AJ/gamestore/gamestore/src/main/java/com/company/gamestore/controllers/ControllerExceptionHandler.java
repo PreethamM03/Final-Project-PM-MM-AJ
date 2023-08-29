ac533b42c4bbd59892effaaa2f50e6d6bdf1a98a
Abby Jackson <ajac@softchoice.com>
2023-08-29 19:06:16 -0400

    Added ControllerAdvice Logic.

---
+++ Final-Project-PM-MM-AJ/gamestore/gamestore/src/main/java/com/company/gamestore/controllers/ControllerExceptionHandler.java
@@ -0,0 +1,67 @@
+package com.company.gamestore.controllers;
+
+import com.company.gamestore.models.CustomErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.webjars.NotFoundException;
+
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    // This method handles IllegalArgument Exceptions
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY) // will return 422 error code
+    public ResponseEntity<CustomErrorResponse> handleGenericNotFoundException(IllegalArgumentException e) {
+        CustomErrorResponse error = new CustomErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY.toString(), e.getMessage());
+        error.setStatus((HttpStatus.UNPROCESSABLE_ENTITY.value()));
+        error.setTimestamp(LocalDateTime.now());
+        ResponseEntity<CustomErrorResponse> responseEntity = new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
+        return responseEntity;
+    }
+
+
+    // This method handles when Method Arguments are not valid such as JSR 303 validation failure
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY) // will return 422 error code
+    public ResponseEntity<List<CustomErrorResponse>> recordValidationError(MethodArgumentNotValidException e) {
+
+        // BindingResult holds the validation errors
+        BindingResult result = e.getBindingResult();
+        // Validation errors are stored in FieldError objects
+        List<FieldError> fieldErrors = result.getFieldErrors();
+
+        // Translate the FieldErrors to CustomErrorResponse
+        List<CustomErrorResponse> errorResponseList = new ArrayList<>();
+
+        for (FieldError fieldError : fieldErrors) {
+            CustomErrorResponse errorResponse = new CustomErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY.toString(), fieldError.getDefaultMessage());
+            errorResponse.setTimestamp(LocalDateTime.now());
+            errorResponse.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+            errorResponseList.add(errorResponse);
+        }
+        // Create and return the ResponseEntity
+        ResponseEntity<List<CustomErrorResponse>> responseEntity = new ResponseEntity<>(errorResponseList, HttpStatus.UNPROCESSABLE_ENTITY);
+        return responseEntity;
+    }
+
+    @ExceptionHandler(value = NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND) // will return 404 error code
+    public ResponseEntity<CustomErrorResponse> notFoundException(NotFoundException e) {
+        CustomErrorResponse error = new CustomErrorResponse(HttpStatus.NOT_FOUND.toString(), e.getMessage());
+        error.setStatus((HttpStatus.NOT_FOUND.value()));
+        error.setTimestamp(LocalDateTime.now());
+        ResponseEntity<CustomErrorResponse> responseEntity = new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        return responseEntity;
+    }
+}
